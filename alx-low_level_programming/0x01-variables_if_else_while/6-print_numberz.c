a4b12970d8fa9ea2e365f05afe74193e2d35e5ce
diboradiborah <diboradiborah@gmail.com>
2023-02-27 23:09:00 -0800

    fter

--- alx-low_level_programming/0x01-variables_if_else_while/6-print_numberz.c
+++ alx-low_level_programming/0x01-variables_if_else_while/6-print_numberz.c
@@ -5,12 +5,17 @@
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	int a;
+	int c;
 
-	for (a = 0; a < 10; a++)
-		printf("%d", a);
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
 	putchar('\n');
 	return (0);
 }
